4bb04a44de5f1a5fd79754fba20bef6f2fee4651
joao.vcleite4 <joao.vcleite4@L130627976W10-1>
2025-02-26 22:01:10 -0300

    Adicionar arquivos de projeto.

---
+++ C_Revisao/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace C_Revisao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a nota do primeiro aluno");
+            double nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a nota do segundo aluno");
+            double nota2 = Convert.ToDouble(Console.ReadLine());
+
+            double media = (nota1 + nota2) / 2;
+
+            if (media >= 7)
+            {
+                Console.WriteLine("Aprovado!");
+            }
+            else if (media < 5)
+            {
+                Console.WriteLine("Reprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Recuperação!");
+            }
+
+        }
+    }
+}  
