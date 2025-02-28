b713749040c1fb117596bff0202662fcc6067b3d
JOAO VITOR CORREIA LEITE <joao.vcleite4@senacsp.edu.br>
2025-02-28 19:19:34 -0300

    Feat: Exercicio 4 corrigido no Console

---
+++ CEx4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CEx4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o Primeiro Numero! ");
+            int num1 = int.Parse(Console.ReadLine()!);
+            Console.Write("\nDigite o Segundo Numero! ");
+            int num2 = int.Parse(Console.ReadLine()!);
+            Console.Write("\nDigite o Terceiro Numero! ");
+            int num3 = int.Parse(Console.ReadLine()!);
+
+            int maior = Math.Max(num1, num2);
+            maior = Math.Max(maior, num3);
+
+            Console.WriteLine($"\nO numero {maior} é o maior valor!");
+        }
+    }
+}
