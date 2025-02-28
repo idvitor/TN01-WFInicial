e47b54079e50041dbf89c9355e1a957e3e66d9c5
JOAO VITOR CORREIA LEITE <joao.vcleite4@senacsp.edu.br>
2025-02-28 19:46:07 -0300

    feat: Exercicio 5 corrigido no console

---
+++ CEx5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CEx5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o Primeiro Numero! ");
+            int num1 = int.Parse(Console.ReadLine()!);
+            Console.Write("\nDigite o Segundo Numero! ");
+            int num2 = int.Parse(Console.ReadLine()!);
+            Console.Write("\nDigite o Terceiro Numero! ");
+            int num3 = int.Parse(Console.ReadLine()!);
+
+            int maior = Math.Max(num1, num2);
+            maior = Math.Max(maior, num3);
+
+            int menor = Math.Min(num1, num2);
+            maior = Math.Min(maior, num3);
+
+
+            Console.WriteLine($"\nO numero {maior} é o maior valor!");
+        }
+    }
+}
---
+++ WFInicial/FormEx5.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WFInicial
+{
+    partial class FormEx5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnMaior = new Button();
+            lblNum3 = new Label();
+            lblNum2 = new Label();
+            lblNum1 = new Label();
+            txtNum3 = new TextBox();
+            txtNum2 = new TextBox();
+            txtNum1 = new TextBox();
+            SuspendLayout();
+            // 
+            // btnMaior
+            // 
+            btnMaior.Location = new Point(185, 209);
+            btnMaior.Name = "btnMaior";
+            btnMaior.Size = new Size(99, 23);
+            btnMaior.TabIndex = 13;
+            btnMaior.Text = "Mostrar Maior";
+            btnMaior.UseVisualStyleBackColor = true;
+            // 
+            // lblNum3
+            // 
+            lblNum3.AutoSize = true;
+            lblNum3.Location = new Point(86, 144);
+            lblNum3.Name = "lblNum3";
+            lblNum3.Size = new Size(60, 15);
+            lblNum3.TabIndex = 12;
+            lblNum3.Text = "Numero 3";
+            // 
+            // lblNum2
+            // 
+            lblNum2.AutoSize = true;
+            lblNum2.Location = new Point(86, 75);
+            lblNum2.Name = "lblNum2";
+            lblNum2.Size = new Size(60, 15);
+            lblNum2.TabIndex = 11;
+            lblNum2.Text = "Numero 2";
+            // 
+            // lblNum1
+            // 
+            lblNum1.AutoSize = true;
+            lblNum1.Location = new Point(86, 16);
+            lblNum1.Name = "lblNum1";
+            lblNum1.Size = new Size(60, 15);
+            lblNum1.TabIndex = 10;
+            lblNum1.Text = "Numero 1";
+            // 
+            // txtNum3
+            // 
+            txtNum3.Location = new Point(172, 136);
+            txtNum3.Name = "txtNum3";
+            txtNum3.Size = new Size(100, 23);
+            txtNum3.TabIndex = 9;
+            // 
+            // txtNum2
+            // 
+            txtNum2.Location = new Point(172, 75);
+            txtNum2.Name = "txtNum2";
+            txtNum2.Size = new Size(100, 23);
+            txtNum2.TabIndex = 8;
+            // 
+            // txtNum1
+            // 
+            txtNum1.Location = new Point(172, 15);
+            txtNum1.Name = "txtNum1";
+            txtNum1.Size = new Size(100, 23);
+            txtNum1.TabIndex = 7;
+            // 
+            // FormEx5
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(371, 247);
+            Controls.Add(btnMaior);
+            Controls.Add(lblNum3);
+            Controls.Add(lblNum2);
+            Controls.Add(lblNum1);
+            Controls.Add(txtNum3);
+            Controls.Add(txtNum2);
+            Controls.Add(txtNum1);
+            Name = "FormEx5";
+            Text = "FormEx5";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnMaior;
+        private Label lblNum3;
+        private Label lblNum2;
+        private Label lblNum1;
+        private TextBox txtNum3;
+        private TextBox txtNum2;
+        private TextBox txtNum1;
+    }
+}
