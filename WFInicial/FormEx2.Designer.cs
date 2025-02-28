59a764e11ce5291215c5bbb3e97d0fe44881911e
JOAO VITOR CORREIA LEITE <joao.vcleite4@senacsp.edu.br>
2025-02-27 22:02:54 -0300

    feat: Exercicio 2 corrigido no Windows Forms

--- WFInicial/FormEx2.Designer.cs
+++ WFInicial/FormEx2.Designer.cs
@@ -28,18 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            lblDiaSemana = new Label();
+            txtDiaSemana = new TextBox();
+            btnEscolher = new Button();
             SuspendLayout();
+            // 
+            // lblDiaSemana
+            // 
+            lblDiaSemana.AutoSize = true;
+            lblDiaSemana.ForeColor = SystemColors.Desktop;
+            lblDiaSemana.Location = new Point(31, 34);
+            lblDiaSemana.Name = "lblDiaSemana";
+            lblDiaSemana.Size = new Size(85, 15);
+            lblDiaSemana.TabIndex = 0;
+            lblDiaSemana.Text = "Dia da Semana";
+            // 
+            // txtDiaSemana
+            // 
+            txtDiaSemana.ForeColor = SystemColors.MenuText;
+            txtDiaSemana.Location = new Point(133, 31);
+            txtDiaSemana.Name = "txtDiaSemana";
+            txtDiaSemana.Size = new Size(100, 23);
+            txtDiaSemana.TabIndex = 1;
+            // 
+            // btnEscolher
+            // 
+            btnEscolher.Location = new Point(266, 30);
+            btnEscolher.Name = "btnEscolher";
+            btnEscolher.Size = new Size(75, 23);
+            btnEscolher.TabIndex = 2;
+            btnEscolher.Text = "Escolher";
+            btnEscolher.UseVisualStyleBackColor = true;
+            btnEscolher.Click += btnEscolher_Click;
             // 
             // FormEx2
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(501, 304);
+            BackColor = SystemColors.HighlightText;
+            ClientSize = new Size(589, 418);
+            Controls.Add(btnEscolher);
+            Controls.Add(txtDiaSemana);
+            Controls.Add(lblDiaSemana);
+            ForeColor = SystemColors.ActiveCaptionText;
             Name = "FormEx2";
             Text = "FormEx2";
+            TransparencyKey = Color.Snow;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblDiaSemana;
+        private TextBox txtDiaSemana;
+        private Button btnEscolher;
     }
 }
