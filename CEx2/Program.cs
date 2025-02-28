50584abc9841029e21c0185af6ad878a3e9d3472
JOAO VITOR CORREIA LEITE <joao.vcleite4@senacsp.edu.br>
2025-02-27 21:16:03 -0300

    feat: Exercicio 2 Corrigido no Console

---
+++ CEx2/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace CEx2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string menu = @"
+----------------------------------------------------------
+1- Domingo
+2- Segunda-Feira 
+3- Terça  -Feira
+4- Quarta -Feira
+5- Quinta -Feira
+6- Sexta  -Feira
+7- Sabado
+----------------------------------------------------------";
+
+            Console.WriteLine(menu);
+            byte opcao = byte.Parse(Console.ReadLine()!);
+
+            switch (opcao)
+            {
+                case 1:
+                    Console.WriteLine("Domingo");
+                    break;
+                case 2:
+                    Console.WriteLine("Segunda - Feira");
+                    break;
+                case 3:
+                    Console.WriteLine("Terça - Feira");
+                    break;
+                case 4:
+                    Console.WriteLine("Quarta - Feira");
+                    break;
+                case 5:
+                    Console.WriteLine("Quinta - Feira");
+                    break;
+                case 6:
+                    Console.WriteLine("Sexta - Feira");
+                    break;
+                case 7:
+                    Console.WriteLine("Sabado");
+                    break;
+                default:
+                    Console.WriteLine("Dia Invalido!");
+                    break;
+
+            }
+        }
+    }
+}
