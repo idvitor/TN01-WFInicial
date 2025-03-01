d760ff8937fe74163c24be6ae8401d15706866f5
JOAO VITOR CORREIA LEITE <joao.vcleite4@senacsp.edu.br>
2025-02-28 21:50:55 -0300

    feat: Exercicio 7 no Windows Forms

---
+++ WFInicial/FormEx7.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace WFInicial
+{
+    partial class FormEx7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtvalor1 = new TextBox();
+            txtvalor2 = new TextBox();
+            txtvalor3 = new TextBox();
+            txtvalor4 = new TextBox();
+            txtvalor5 = new TextBox();
+            txtvalor6 = new TextBox();
+            txtvalor7 = new TextBox();
+            lblnome1 = new Label();
+            lblnome2 = new Label();
+            lblnome3 = new Label();
+            lblnome4 = new Label();
+            lblnome5 = new Label();
+            lblnome6 = new Label();
+            lblnome7 = new Label();
+            btntotal = new Button();
+            SuspendLayout();
+            // 
+            // txtvalor1
+            // 
+            txtvalor1.Location = new Point(211, 43);
+            txtvalor1.Name = "txtvalor1";
+            txtvalor1.Size = new Size(100, 23);
+            txtvalor1.TabIndex = 0;
+            // 
+            // txtvalor2
+            // 
+            txtvalor2.Location = new Point(211, 85);
+            txtvalor2.Name = "txtvalor2";
+            txtvalor2.Size = new Size(100, 23);
+            txtvalor2.TabIndex = 1;
+            // 
+            // txtvalor3
+            // 
+            txtvalor3.Location = new Point(211, 138);
+            txtvalor3.Name = "txtvalor3";
+            txtvalor3.Size = new Size(100, 23);
+            txtvalor3.TabIndex = 2;
+            // 
+            // txtvalor4
+            // 
+            txtvalor4.Location = new Point(211, 186);
+            txtvalor4.Name = "txtvalor4";
+            txtvalor4.Size = new Size(100, 23);
+            txtvalor4.TabIndex = 3;
+            // 
+            // txtvalor5
+            // 
+            txtvalor5.Location = new Point(211, 245);
+            txtvalor5.Name = "txtvalor5";
+            txtvalor5.Size = new Size(100, 23);
+            txtvalor5.TabIndex = 4;
+            // 
+            // txtvalor6
+            // 
+            txtvalor6.Location = new Point(211, 292);
+            txtvalor6.Name = "txtvalor6";
+            txtvalor6.Size = new Size(100, 23);
+            txtvalor6.TabIndex = 5;
+            // 
+            // txtvalor7
+            // 
+            txtvalor7.Location = new Point(211, 341);
+            txtvalor7.Name = "txtvalor7";
+            txtvalor7.Size = new Size(100, 23);
+            txtvalor7.TabIndex = 6;
+            // 
+            // lblnome1
+            // 
+            lblnome1.AutoSize = true;
+            lblnome1.Location = new Point(94, 43);
+            lblnome1.Name = "lblnome1";
+            lblnome1.Size = new Size(56, 15);
+            lblnome1.TabIndex = 7;
+            lblnome1.Text = "morango";
+            // 
+            // lblnome2
+            // 
+            lblnome2.AutoSize = true;
+            lblnome2.Location = new Point(94, 88);
+            lblnome2.Name = "lblnome2";
+            lblnome2.Size = new Size(42, 15);
+            lblnome2.TabIndex = 8;
+            lblnome2.Text = "laranja";
+            // 
+            // lblnome3
+            // 
+            lblnome3.AutoSize = true;
+            lblnome3.Location = new Point(94, 141);
+            lblnome3.Name = "lblnome3";
+            lblnome3.Size = new Size(26, 15);
+            lblnome3.TabIndex = 9;
+            lblnome3.Text = "uva";
+            // 
+            // lblnome4
+            // 
+            lblnome4.AutoSize = true;
+            lblnome4.Location = new Point(94, 194);
+            lblnome4.Name = "lblnome4";
+            lblnome4.Size = new Size(48, 15);
+            lblnome4.TabIndex = 10;
+            lblnome4.Text = "mamao";
+            // 
+            // lblnome5
+            // 
+            lblnome5.AutoSize = true;
+            lblnome5.Location = new Point(94, 245);
+            lblnome5.Name = "lblnome5";
+            lblnome5.Size = new Size(46, 15);
+            lblnome5.TabIndex = 11;
+            lblnome5.Text = "banana";
+            lblnome5.Click += label5_Click;
+            // 
+            // lblnome6
+            // 
+            lblnome6.AutoSize = true;
+            lblnome6.Location = new Point(94, 292);
+            lblnome6.Name = "lblnome6";
+            lblnome6.Size = new Size(45, 15);
+            lblnome6.TabIndex = 12;
+            lblnome6.Text = "ameixa";
+            lblnome6.Click += label6_Click;
+            // 
+            // lblnome7
+            // 
+            lblnome7.AutoSize = true;
+            lblnome7.Location = new Point(94, 341);
+            lblnome7.Name = "lblnome7";
+            lblnome7.Size = new Size(41, 15);
+            lblnome7.TabIndex = 13;
+            lblnome7.Text = "amora";
+            // 
+            // btntotal
+            // 
+            btntotal.Location = new Point(148, 390);
+            btntotal.Name = "btntotal";
+            btntotal.Size = new Size(97, 23);
+            btntotal.TabIndex = 14;
+            btntotal.Text = "Valor menor";
+            btntotal.UseVisualStyleBackColor = true;
+            btntotal.Click += btn1_Click;
+            // 
+            // FormEx7
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(455, 425);
+            Controls.Add(btntotal);
+            Controls.Add(lblnome7);
+            Controls.Add(lblnome6);
+            Controls.Add(lblnome5);
+            Controls.Add(lblnome4);
+            Controls.Add(lblnome3);
+            Controls.Add(lblnome2);
+            Controls.Add(lblnome1);
+            Controls.Add(txtvalor7);
+            Controls.Add(txtvalor6);
+            Controls.Add(txtvalor5);
+            Controls.Add(txtvalor4);
+            Controls.Add(txtvalor3);
+            Controls.Add(txtvalor2);
+            Controls.Add(txtvalor1);
+            Name = "FormEx7";
+            Text = "FormEx7";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtvalor1;
+        private TextBox txtvalor2;
+        private TextBox txtvalor3;
+        private TextBox txtvalor4;
+        private TextBox txtvalor5;
+        private TextBox txtvalor6;
+        private TextBox txtvalor7;
+        private Label lblnome1;
+        private Label lblnome2;
+        private Label lblnome3;
+        private Label lblnome4;
+        private Label lblnome5;
+        private Label lblnome6;
+        private Label lblnome7;
+        private Button btntotal;
+    }
+}
---
+++ WFInicial/FormEx7.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFInicial
+{
+    public partial class FormEx7 : Form
+    {
+        public FormEx7()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
